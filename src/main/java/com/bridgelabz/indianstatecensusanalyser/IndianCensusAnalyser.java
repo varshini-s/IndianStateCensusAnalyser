9e11cfec995d146edb3c45a635475f93ab5b7894
varshini <dreamvarshini@gmail.com>
2021-09-21 21:36:38 +0530

    ADDED: function to read and count state code data

--- src/main/java/com/bridgelabz/indianstatecensusanalyser/IndianCensusAnalyser.java
+++ src/main/java/com/bridgelabz/indianstatecensusanalyser/IndianCensusAnalyser.java
@@ -56,5 +56,45 @@ public class IndianCensusAnalyser
 		}
 	}
 
+	public int loadIndianStateCode(String csvFilePath) throws CensusAnalyserException 
+	{
+		try ( Reader reader = Files.newBufferedReader(Paths.get(csvFilePath)))
+		{
+
+			Pattern pattern = Pattern.compile(FILE_NAME_PATTERN);
+			if(pattern.matcher(csvFilePath).matches()==false)
+			{
+				throw new CensusAnalyserException("Invalid file type",
+												  CensusAnalyserException.ExceptionType.INCORRECT_FILE_TYPE);
+			}
+
+			CsvToBeanBuilder<IndiaStateCodeCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+			csvToBeanBuilder.withType(IndiaStateCodeCSV.class);
+			csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+			CsvToBean<IndiaStateCodeCSV> csvToBean = csvToBeanBuilder.build();
+			Iterator<IndiaStateCodeCSV> censusCSVIterator  = csvToBean.iterator();
+
+			Iterable<IndiaStateCodeCSV> iterable = () -> censusCSVIterator;
+
+			int numberOfEntries = (int) StreamSupport.stream(iterable.spliterator(), false).count();
+			return numberOfEntries;
+
+		}
+		catch (IOException e) 
+		{
+			throw new CensusAnalyserException(e.getMessage(),
+											  CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+		} catch (IllegalStateException e) 
+		{
+			throw new CensusAnalyserException(e.getMessage(),
+											  CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
+		}
+		catch (RuntimeException e) 
+		{
+			throw new CensusAnalyserException(e.getMessage(),
+											  CensusAnalyserException.ExceptionType.CSV_FILE_INTERNAL_ISSUES);
+		}
+	}
+	
 
 }
